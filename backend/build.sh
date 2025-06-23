aa23271ba5c6463f48bf24be0ce5d3ae55cc37bf
JC <jc@languagebridge.xyz>
2025-06-23 23:09:50 +0800

    update build

--- backend/build.sh
+++ backend/build.sh
@@ -2,9 +2,9 @@
 set -e
 
 # 配置参数
-PROJECT_DIR="/home/mystks_api"         # 代码所在目录
-OUTPUT_DIR="/app/stonks-api"           # 编译输出目录
-BINARY_NAME="stonks-api"               # 生成的二进制文件名称
+PROJECT_DIR="/data/langbridge/source/languagebridge/backend"         # 代码所在目录
+OUTPUT_DIR="/data/langbridge/server"          # 编译输出目录
+BINARY_NAME="lb-api"               # 生成的二进制文件名称
 
 echo "切换到项目目录：$PROJECT_DIR"
 cd "$PROJECT_DIR"
